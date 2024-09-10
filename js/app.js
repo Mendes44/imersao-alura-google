fd3a0fc8d2df80a03f9e3e71ba0de46737020959
Mendes44 <marcosmendesm10@gmail.com>
2024-09-09 22:06:00 -0300

    Implementando a Funcionalidade de Busca

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log(tecnologias);
+
+// Buscar o elemento pelo ID
+let resultado = document.getElementById ( "mostrar-resultado" );
+
+resultado.innerHTML = `
+ <div class="intem-resultado">
+    <h2>
+        <a href="#" target="_blank">Teste JavaScript</a>
+    </h2>
+    <p class="descricao-meta">teste de injeção de html através do javaScript</p>
+    <a href="">Mais Informações</a>
+</div>
+`
