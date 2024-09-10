26c9230cd825d2c156c0f68764d5152b1fc15fd6
Mendes44 <marcosmendesm10@gmail.com>
2024-09-09 22:48:40 -0300

    Implementando Função OnClick

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
-console.log(tecnologias);
-
-// Buscar o elemento pelo ID
-let resultado = document.getElementById ( "mostrar-resultado" );
-
-// Para cada dado dentro da lista de Tecnologias
-for ( let dado of tecnologias ) {
-    resultado.innerHTML = `
-     <div class="intem-resultado">
-        <h2>
+// Função que e acionado pelo botão pesquisar
+function pesquisar() {
+  
+    // Buscar o elemento pelo ID e armazenar em uma variável
+    let dados = document.getElementById("mostrar-resultado");
+    
+    // Criar uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    
+    // Iterar sobre cada tecnologia na lista de tecnologias
+    for (let dado of tecnologias) {
+      // Criar um novo elemento div para cada tecnologia e adicionar informações
+      resultados += `
+        <div class="intem-resultado">
+          <h2>
             <a href="#" target="_blank">${dado.nome}</a>
-        </h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}">Mais Informações</a>
-    </div>
-    `
-}
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}">Mais Informações</a>
+        </div>
+      `
+    };
+    
+    // Atribuir os resultados gerados ao elemento com o ID "mostrar-resultado"
+    dados.innerHTML = resultados;
+  };
